d908bf5a99b35ac4ff7a4170a3030e631437ddf9
Eshithareddy41 <eshureddy20@gmail.com>
2026-04-08 01:12:27 +0530

    Implement bootloader to load kernel from disc

--- boot.asm
+++ boot.asm
@@ -2,12 +2,26 @@
 [org 0x7c00]
 
 start:
-  mov ah,0x0e
-  mov al,'H'
-  int 0x10
+    mov ah,0x0e
+    mov al,'B'
+    int 0x10          ; print B to show bootloader started
 
-loop:
-  jmp loop
+    xor ax,ax
+    mov es,ax
+    mov bx,0x1000     ; kernel load address
+
+    mov ah,0x02       ; BIOS disk read
+    mov al,20         ; number of sectors
+    mov ch,0
+    mov cl,2
+    mov dh,0
+    mov dl,0x80
+    int 0x13
+
+    jmp 0x0000:0x1000 ; jump to kernel
+
+hang:
+    jmp hang
 
 times 510-($-$$) db 0
 dw 0xaa55

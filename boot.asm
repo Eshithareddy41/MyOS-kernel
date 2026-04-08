5ed94af96626f3e15c7d1b90ec7d612ac9decbe3
Eshithareddy41 <eshureddy20@gmail.com>
2026-04-08 22:34:26 +0530

    Improve bootloader for kernel loading and erroe handling

--- boot.asm
+++ boot.asm
@@ -1,27 +1,22 @@
-[bits 16]
-[org 0x7c00]
+[org 0x7C00]
 
-start:
-    mov ah,0x0e
-    mov al,'B'
-    int 0x10          ; print B to show bootloader started
+; Print 'B' to confirm bootloader works
+mov ah, 0x0E
+mov al, 'B'
+int 0x10
 
-    xor ax,ax
-    mov es,ax
-    mov bx,0x1000     ; kernel load address
+; Load kernel (sector 2)
+mov ah, 0x02
+mov al, 1
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, 0x80
+mov bx, 0x1000
+int 0x13
 
-    mov ah,0x02       ; BIOS disk read
-    mov al,20         ; number of sectors
-    mov ch,0
-    mov cl,2
-    mov dh,0
-    mov dl,0x80
-    int 0x13
-
-    jmp 0x0000:0x1000 ; jump to kernel
-
-hang:
-    jmp hang
+; Jump to kernel
+jmp 0x0000:0x1000
 
 times 510-($-$$) db 0
-dw 0xaa55
+dw 0xAA55
